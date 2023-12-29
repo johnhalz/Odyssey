db56166c99a2e5cc9596a8cf39d9b0d106bc83d4
John Halazonetis <john.halazonetis@scandit.com>
2023-12-29 12:39:05 -0700

    Switched to postgres database

--- Odyssey/settings.py
+++ Odyssey/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Odyssey.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
