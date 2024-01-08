9d085833a7bf35bfff47fc4a25232232a41a52c1
John Halazonetis <john.halazonetis@scandit.com>
2024-01-08 09:09:01 +0100

    Adding REST API

--- Odyssey/settings.py
+++ Odyssey/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'odyssey_production.apps.OdysseyProductionConfig'
+    'odyssey_production.apps.OdysseyProductionConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
