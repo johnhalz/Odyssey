2f879965ef3b17e885e6acc293d6bf3b1f9d6b88
John Halazonetis <john.halazonetis@icloud.com>
2024-04-13 00:19:14 +0200

    Updated settings and packages

--- Odyssey/settings.py
+++ Odyssey/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'mypassword',
+        'PASSWORD': 'odysseypassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
