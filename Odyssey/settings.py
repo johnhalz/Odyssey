d84f9df74e8e5e999ea7e23ae16b5e0801a02852
John Halazonetis <john.halazonetis@icloud.com>
2024-04-13 00:19:50 +0200

    Squashed commit of the following:
    commit 2f879965ef3b17e885e6acc293d6bf3b1f9d6b88
    Author: John Halazonetis <john.halazonetis@icloud.com>
    Date:   Sat Apr 13 00:19:14 2024 +0200
    
        Updated settings and packages

--- Odyssey/settings.py
+++ Odyssey/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'mypassword',
+        'PASSWORD': 'odysseypassword',
         'HOST': 'localhost',
         'PORT': '5432',
     }
