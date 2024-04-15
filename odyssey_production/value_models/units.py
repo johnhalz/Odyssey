3324a325c4d360a0a36ecf52c8ff95b842791c1d
John Halazonetis <john.halazonetis@icloud.com>
2024-04-15 19:35:01 +0200

    Added seed data for units and unit types

--- odyssey_production/value_models/units.py
+++ odyssey_production/value_models/units.py
@@ -10,10 +10,21 @@ class UnitType(models.Model):
 
 class Unit(models.Model):
     name = models.CharField(max_length=30)
+    plural_name = models.CharField(max_length=30, blank=True, null=True)
+    space_after_value = models.BooleanField(default=True)
     type = models.ForeignKey(UnitType, on_delete=models.CASCADE)
     base_unit = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-    conversion_to_base = models.CharField(max_length=80, default="x")
     abbreviation = models.CharField(max_length=10)
+    """
+    For conversion, we will use the formula:
+    y = ((x + x_offset) * multiplicand / denominator) + y_offset
+    
+    We create the fields to store these constants
+    """
+    x_offset = models.DecimalField(max_digits=19, decimal_places=10, default=0)
+    y_offset = models.DecimalField(max_digits=19, decimal_places=10, default=0)
+    multiplicand = models.DecimalField(max_digits=19, decimal_places=10, default=1)
+    denominator = models.DecimalField(max_digits=19, decimal_places=10, default=1)
 
     def __str__(self):
         return f"{self.name} ({self.abbreviation})"
