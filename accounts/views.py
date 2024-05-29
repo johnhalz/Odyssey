24734900e64327597536cade65afa089e474d7c7
John Halazonetis <john.halazonetis@icloud.com>
2024-05-29 20:27:28 +0200

    Fixed warning where serializer was not guessed

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import login
-
-from rest_framework import generics, permissions
-from rest_framework.authtoken.serializers import AuthTokenSerializer
-
+from knox.auth import AuthToken, TokenAuthentication
 from knox.views import LoginView as KnoxLoginView
+from knox.views import LogoutAllView as KnoxLogoutAllView
+from knox.views import LogoutView as KnoxLogoutView
+from rest_framework import generics, permissions, serializers, status
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.response import Response
 
-from accounts.serializers import UserSerializer, AuthSerializer
+from accounts.serializers import AuthSerializer, UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -21,16 +23,29 @@ class LoginView(KnoxLoginView):
     def post(self, request, format=None):
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
+        user = serializer.validated_data["user"]
         login(request, user)
-        return super(LoginView, self).post(request, format=None)    
+        return super(LoginView, self).post(request, format=None)
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticated user"""
+
     serializer_class = UserSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_object(self):
         """Retrieve and return authenticated user"""
         return self.request.user
+
+
+class EmptySerializer(serializers.Serializer):
+    pass
+
+
+class LogoutView(KnoxLogoutView):
+    serializer_class = EmptySerializer
+
+
+class LogoutAllView(KnoxLogoutAllView):
+    serializer_class = EmptySerializer
